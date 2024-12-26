818ea8c2757989f8d21b9ed4354c01452ab1b13a
Jacob Petersen <jaschapet@gmail.com>
2024-12-26 22:19:25 +0000

    Added update script, barebones functionality

--- ionos_ddns_update.py
+++ ionos_ddns_update.py
@@ -9,23 +9,32 @@ load_dotenv()
 # Import API private and public key from .env file
 API_KEY = os.getenv('API_KEY')
 API_KEY_PUBLIC = os.getenv('API_KEY_PUBLIC')
+ZONE_ID = os.getenv('ZONE_ID')
+DOMAIN_ID = os.getenv('DOMAIN_ID')
 
 # Ionos DNS zones API base URL
-url = 'https://api.hosting.ionos.com/dns/v1/zones/' + '20a908db-14a9-11ec-81e7-0a5864444eb9'
+url = 'https://api.hosting.ionos.com/dns/v1/zones/' + ZONE_ID + '/records/' + DOMAIN_ID
+
+# Get the server's IP. Simple API call to ipify.org
+ip = requests.get('https://api.ipify.org?format=json').json()['ip']
+print('Public IP address: {}'.format(ip))
 
 request_headers = {
     'X-API-Key': '{}.{}'.format(API_KEY_PUBLIC, API_KEY)
 }
 
 request_parameters = {
-    'zoneId': '20a908db-14a9-11ec-81e7-0a5864444eb9',
-    'recordType': 'A'
+    'zoneId': ZONE_ID,
+    'recordID': DOMAIN_ID
 }
 
 request_body = {
-
+    'content': ip,
+    'disabled': False,
+    'ttl': 3600,
+    'prio': 0
 }
 
-response = requests.get(url, headers=request_headers, params=request_parameters)
+response = requests.put(url, headers=request_headers, params=request_parameters, json=request_body)
 print('Code: {}'.format(response.status_code))
 print(json.dumps(response.json(), indent=2))
